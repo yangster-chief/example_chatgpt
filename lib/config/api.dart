2ae137c5a5ee2ecc214e9b8ecdc4bc9e82d94112
thunder-ramgee <38216680+yangster-chief@users.noreply.github.com>
2023-11-04 04:04:26 +0900

    add api

---
+++ lib/config/api.dart
@@ -0,0 +1,34 @@
+import 'dart:convert';
+
+import 'package:example_chatgpt/config/constants.dart';
+import 'package:http/http.dart' as http;
+
+///
+/// example_chatgpt
+/// File Name: api
+/// Created by sujangmac on 11/4/23
+///
+/// Description:
+///
+const apiUrl = 'https://api.openai.com/v1/chat/completions';
+
+Future<String> generateText(String prompt) async {
+  final response = await http.post(
+    Uri.parse(apiUrl),
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': 'Bearer $kSecretKey'
+    },
+    body: jsonEncode({
+      'model': 'gpt-3.5-turbo',
+      'messages': [
+        {'role': 'system', 'content': 'You are a helpful assistant.'},
+        {'role': 'user', 'content': 'Hello!'}
+      ]
+    }),
+  );
+
+  Map<String, dynamic> data = jsonDecode(utf8.decode(response.bodyBytes));
+
+  return data['choices'][0]['message']['content'];
+}
---
+++ test/api_test.dart
@@ -0,0 +1,18 @@
+import 'package:example_chatgpt/config/api.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+///
+/// example_chatgpt
+/// File Name: api_test
+/// Created by sujangmac on 11/4/23
+///
+/// Description:
+///
+
+void main() {
+  test('generateText', () async {
+    final response = await generateText('Hello, my name is');
+    print(response);
+    expect(response, isNotEmpty);
+  });
+}
